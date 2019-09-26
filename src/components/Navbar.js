afab228365f13274c01f405a61c25700796a040f
Przemysław Kadjam <przemyslaw.kadjam@gmail.com>
2019-09-26 15:55:08 +0200

    component structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>
+        <span>🥪</span> Dishes Form
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
